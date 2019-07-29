f81136ee27d754c9fb6ce01aabbdfc3325be7b68
Adnan Afsari <aarahul1991@gmail.com>
2019-07-29 20:04:33 +0600

    battleship model created

---
+++ src/js/Ship.js
@@ -0,0 +1,21 @@
+let allShips = {
+  regant: {size: 1},
+  galaxy: {size: 2},
+  summit: {size: 3},
+  edge: {size: 4},
+  oasis: {size: 5},
+};
+​
+const Ship = (name) => {
+​
+  const {size} = allShips[name];
+​
+  let countHit = 0;
+  const hit = () => ++countHit;
+​
+  const isSunk = () => countHit === size;
+​
+  return {name, size, countHit, hit, isSunk};
+};
+​
+export {allShips, Ship};
